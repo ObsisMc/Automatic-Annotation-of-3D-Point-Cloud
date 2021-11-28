4214761289f6534f5ac88aaa19027d5b21cf844a
zrh_local <zrh735847540@outlook.com>
2021-11-28 23:16:48 +0800

    add function of visualize module

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # InnovativePractice1_SUSTech
 
-visualize model is cloned from https://github.com/junming259/Partial_Point_Clouds_Generation.
+Visualize model is cloned from https://github.com/junming259/Partial_Point_Clouds_Generation but there are some changes which allows the code to run in script.
 
--- visualize/visualize.py
+++ visualize/visualize.py
@@ -7,13 +7,13 @@ from utils import boxes_to_corners_3d
 
 
 def visual_right_scale(pos, ax):
-    max_range = np.array([pos[:, 0].max()-pos[:, 0].min(),
-                          pos[:, 1].max()-pos[:, 1].min(),
-                          pos[:, 2].max()-pos[:, 2].min()]).max() / 2.0
+    max_range = np.array([pos[:, 0].max() - pos[:, 0].min(),
+                          pos[:, 1].max() - pos[:, 1].min(),
+                          pos[:, 2].max() - pos[:, 2].min()]).max() / 2.0
 
-    mid_x = (pos[:, 0].max()+pos[:, 0].min()) * 0.5
-    mid_y = (pos[:, 1].max()+pos[:, 1].min()) * 0.5
-    mid_z = (pos[:, 2].max()+pos[:, 2].min()) * 0.5
+    mid_x = (pos[:, 0].max() + pos[:, 0].min()) * 0.5
+    mid_y = (pos[:, 1].max() + pos[:, 1].min()) * 0.5
+    mid_z = (pos[:, 2].max() + pos[:, 2].min()) * 0.5
 
     # make scale look equal
     ax.set_xlim(mid_x - max_range, mid_x + max_range)
@@ -49,43 +49,58 @@ def sample(pos, n):
     return pos[idx]
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--i", type=int, default=0, help="points_{i}.npy")
-parser.add_argument("--idx", type=str, default='000936',
-                    help='specify data index: {idx}.bin')
-parser.add_argument('--category', type=str, default='car',
-                    help='specify the category' + 
-                        '{ \
-                            Car, \
-                            Van, \
-                            Truck, \
-                            Pedestrian, \
-                            Person_sitting, \
-                            Cyclist, \
-                            Tram \
-                        }')
-args = parser.parse_args()
+class pseudoargs():
+    def __init__(self, idx, i, cate):
+        self.idx = idx
+        self.i = i
+        self.category = cate
 
-######### Visualize in matplotlib ########
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
 
-# pts_path = 'output/points.npy'
-# bbox_path = 'output/bboxes.npy'
-pts_path = 'output/{}_{}_point_{}.npy'.format(args.idx, args.category, args.i)
-bbox_path = 'output/{}_{}_bbox_{}.npy'.format(args.idx, args.category, args.i)
-pts = np.load(pts_path).reshape(-1, 3)
-bbox = np.load(bbox_path).reshape(-1, 7)
-corners3d = boxes_to_corners_3d(bbox)
-# pts = sample(pts, 5000)
+def main(shrun=False, i=0, idx='000936', category='car'):
+    if not shrun:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--i", type=int, default=0, help="points_{i}.npy")
+        parser.add_argument("--idx", type=str, default='000936',
+                            help='specify data index: {idx}.bin')
+        parser.add_argument('--category', type=str, default='car',
+                            help='specify the category' +
+                                 '{ \
+                                     Car, \
+                                     Van, \
+                                     Truck, \
+                                     Pedestrian, \
+                                     Person_sitting, \
+                                     Cyclist, \
+                                     Tram \
+                                 }')
+        args = parser.parse_args()
+    else:
+        args = pseudoargs(idx, i, category)
 
-ax.scatter(pts[:, 0], pts[:, 1], pts[:, 2], s=5, c='g', lw=0, alpha=1)
-visualize_3d_boxes(corners3d, ax)
+    ######### Visualize in matplotlib ########
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-visual_right_scale(corners3d.reshape(-1, 3), ax)
-ax.title.set_text(args.category)
-ax.view_init(elev=120., azim=-90)  #视角转换
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-plt.show()
+    # pts_path = 'output/points.npy'
+    # bbox_path = 'output/bboxes.npy'
+    pts_path = 'output/{}_{}_point_{}.npy'.format(args.idx, args.category, args.i)
+    bbox_path = 'output/{}_{}_bbox_{}.npy'.format(args.idx, args.category, args.i)
+    pts = np.load(pts_path).reshape(-1, 3)
+    bbox = np.load(bbox_path).reshape(-1, 7)
+    corners3d = boxes_to_corners_3d(bbox)
+    # pts = sample(pts, 5000)
+
+    ax.scatter(pts[:, 0], pts[:, 1], pts[:, 2], s=5, c='g', lw=0, alpha=1)
+    visualize_3d_boxes(corners3d, ax)
+
+    visual_right_scale(corners3d.reshape(-1, 3), ax)
+    ax.title.set_text(args.category)
+    ax.view_init(elev=120., azim=-90)  # 视角转换
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
