16d6af053792080c7a53fda041d4d0b7acb6d09a
jidantang55 <83770280+jidantang55@users.noreply.github.com>
2021-12-18 15:44:00 +0800

    MyDataSet

--- OurNet/MyDataSet.py
+++ OurNet/MyDataSet.py
@@ -1,18 +1,21 @@
-from torch.utils.data import Dataset
+import os
+
 import numpy as np
+from torch.utils.data import Dataset
 
 
 class MyDataSet(Dataset):
     def __init__(self, data_path):
-        self.data = self.initdata(data_path)
-        pass
+        super(MyDataSet, self).__init__()
+        self.data_path = data_path
+        self.data_name = os.listdir(data_path)  # A list of names for data files
+        self.data_num = len(self.data_name)  # The number of data files
 
     def __getitem__(self, item):
-        pass
+        data_name = self.data_name[item]
+        data_path = os.path.join(self.data_path, data_name)
+        data = np.load(data_path)
+        return data
 
     def __len__(self):
-        return len(self.data)
-
-    def initdata(self, data_path):
-        data = None
-        return data
+        return self.data_num
