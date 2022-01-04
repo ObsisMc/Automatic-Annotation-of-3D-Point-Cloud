46caf826d41d6810fe51d4b2d70454c017a866d1
jidantang55 <83770280+jidantang55@users.noreply.github.com>
2022-01-04 14:20:26 +0800

    。。。。。

--- OurNet/MyModel.py
+++ OurNet/MyModel.py
@@ -78,10 +78,9 @@ class PointNetCls(nn.Module):
         self.feat = PointNetfeat()
         self.fc1 = nn.Linear(2048, 1024)
         self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 256)
-        self.fc4 = nn.Linear(256, 2)
-        self.fc5 = nn.Linear(512, 256)
-        self.fc6 = nn.Linear(256, k)
+        self.fc3 = nn.Linear(512, 1)
+        # self.fc5 = nn.Linear(512, 256)
+        # self.fc6 = nn.Linear(256, k)
         self.dropout = nn.Dropout(p=0.3)
         # self.bn1 = nn.BatchNorm1d(512)
         # self.bn2 = nn.BatchNorm1d(256)
@@ -94,7 +93,34 @@ class PointNetCls(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.dropout(self.fc2(x)))
         out1 = F.relu(self.fc3(x))
-        out1 = F.sigmoid(self.fc4(out1))
-        out2 = F.relu(self.fc5(x))
-        out2 = self.fc6(out2)
-        return out1, out2
+        out1 = F.sigmoid(out1)
+        # out2 = self.fc5(x)
+        # out2 = self.fc6(out2)
+        return out1
+
+
+class PointNetPred(nn.Module):
+    def __init__(self, k=4):
+        super(PointNetPred, self).__init__()
+        self.feat = PointNetfeat()
+        self.fc1 = nn.Linear(2048, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, k)
+        # self.fc4 = nn.Linear(256, k)
+        # self.fc5 = nn.Linear(512, 256)
+        # self.fc6 = nn.Linear(256, k)
+        self.dropout = nn.Dropout(p=0.3)
+        # self.bn1 = nn.BatchNorm1d(512)
+        # self.bn2 = nn.BatchNorm1d(256)
+        self.relu = nn.ReLU()
+
+    def forward(self, x1, x2):
+        x1, trans1, trans_feat1 = self.feat(x1)
+        x2, trans2, trans_feat2 = self.feat(x2)
+        x = torch.cat((x1, x2), 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.dropout(self.fc2(x)))
+        # out1 = F.relu(self.fc3(x))
+        # out1 = F.sigmoid(self.fc4(out1))
+        out2 = self.fc3(x)
+        return out2
--- OurNet/MyTraining.py
+++ OurNet/MyTraining.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 import torch.utils.data
 
 from MyDataSet import MyDataSet
-from MyModel import PointNetCls
+from MyModel import PointNetCls, PointNetPred
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 parser = argparse.ArgumentParser()
@@ -21,7 +21,7 @@ parser.add_argument(
     '--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument(
     '--nepoch', type=int, default=250, help='number of epochs to train for')
-parser.add_argument('--outf', type=str, default='checkpoints',
+parser.add_argument('--outf', type=str, default='../checkpoints',
                     help='output folder')
 parser.add_argument('--model', type=str, default='', help='model path')
 parser.add_argument('--dataset', type=str,
@@ -67,19 +67,26 @@ def main():
         pass
 
     classifier = PointNetCls()
+    predictor = PointNetPred()
 
     if opt.model != '':
         classifier.load_state_dict(torch.load(opt.model))
 
-    optimizer = optim.Adam(classifier.parameters(), lr=0.0001, betas=(0.9, 0.999))
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+    optimizer1 = optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
+    optimizer2 = optim.SGD(predictor.parameters(), lr=0.01, momentum=0.9)
+    scheduler1 = optim.lr_scheduler.StepLR(optimizer1, step_size=10, gamma=0.5)
+    scheduler2 = optim.lr_scheduler.StepLR(optimizer2, step_size=10, gamma=0.5)
     if not opt.cpu:
         classifier.cuda()
+        predictor.cuda()
 
     num_batch = len(train_dataset) / opt.batchSize
-
+    min_loss = 1e10
     for epoch in range(opt.nepoch):
-        total_loss = 0
+        total_loss1 = 0
+        total_loss1_1 = 0
+        total_loss1_2 = 0
+        total_loss2 = 0
         for i, data in enumerate(train_dataloader, 0):
             points, target = data
             points1 = points[0].transpose(2, 1)
@@ -89,18 +96,35 @@ def main():
             points2 = points2.type(torch.FloatTensor)
             if not opt.cpu:
                 points1, points2, target = points1.cuda(), points2.cuda(), target.cuda()
-            optimizer.zero_grad()
+            optimizer1.zero_grad()
+            optimizer2.zero_grad()
             classifier = classifier.train()
-            pred1, pred2 = classifier(points1, points2)
-            target_cls = target[4].to(torch.long).unsqueeze(0)
-            loss1 = F.cross_entropy(pred1, target_cls).to(torch.float32)
-            loss2 = F.mse_loss(pred2, target[:4].unsqueeze(0).to(torch.float32)) * 20
+            pred1 = classifier(points1, points2)
+            predictor = predictor.train()
+            pred2 = predictor(points1, points2)
+            target_cls = target[4].unsqueeze(0).unsqueeze(0)
+            loss1 = F.binary_cross_entropy_with_logits(pred1, target_cls).to(torch.float32)
+            loss2 = F.smooth_l1_loss(pred2, target[:4].unsqueeze(0).to(torch.float32)) * 30
             # if the actual value of target[4] is 0, then the loss2 is 0
-            loss2 = loss2 * (target[4] != 0).float()
+            loss2 = loss2 * (target[4].to(torch.long) != 0).float()
             loss = loss1 + loss2
-            loss.backward()
-            optimizer.step()
-        scheduler.step()
+            if target[4].to(torch.long) != 0:
+                loss1.backward()
+                # loss2.backward()
+            else:
+                loss1.backward()
+            total_loss1_1 += loss1.item()
+            total_loss1_2 += loss2.item()
+            total_loss1 += loss.item()
+            optimizer1.step()
+            optimizer2.step()
+        total_loss1_1 /= len(train_dataset)
+        total_loss1_2 /= len(train_dataset)
+        total_loss1 /= len(train_dataset)
+        print('train: epoch %d, loss1: %f, loss2: %f, average loss: %f' % (
+        epoch, total_loss1_1, total_loss1_2, total_loss1))
+        scheduler1.step()
+        scheduler2.step()
         # print('[%d: %d/%d] train loss1: %f  loss2: %f  total loss: %f' % (
         #     epoch, i, len(train_dataset), loss1.item(), loss2.item(), loss.item()))
         for j, data in enumerate(valid_dataloader, 0):
@@ -112,21 +136,27 @@ def main():
             points2 = points2.type(torch.FloatTensor)
             if not opt.cpu:
                 points1, points2, target = points1.cuda(), points2.cuda(), target.cuda()
-            optimizer.zero_grad()
+            optimizer1.zero_grad()
+            optimizer2.zero_grad()
             classifier = classifier.train()
-            pred1, pred2 = classifier(points1, points2)
-            target_cls = target[4].to(torch.long).unsqueeze(0)
-            loss1 = F.cross_entropy(pred1, target_cls).to(torch.float32)
-            loss2 = F.mse_loss(pred2, target[:4].unsqueeze(0).to(torch.float32))
-            loss2 = loss2 * (target[4] != 0).float() * 20
+            pred1 = classifier(points1, points2)
+            predictor = predictor.train()
+            pred2 = predictor(points1, points2)
+            target_cls = target[4].unsqueeze(0).unsqueeze(0)
+            loss1 = F.binary_cross_entropy_with_logits(pred1, target_cls).to(torch.float32)
+            loss2 = F.smooth_l1_loss(pred2, target[:4].unsqueeze(0).to(torch.float32)) * 30
+            # if the actual value of target[4] is 0, then the loss2 is 0
+            loss2 = loss2 * (target[4].to(torch.long) != 0).float()
             loss = loss1 + loss2
-            total_loss += loss.item()
-        total_loss /= len(valid_dataset)
-        print('epoch %d, average loss: %f' % (epoch, total_loss))
-        with open(opt.outf + '/log.txt', 'a') as f:
-            f.write('epoch: %d, loss: %f\n' % (epoch, total_loss))
-
-        torch.save(classifier.state_dict(), '%s/model_%d_%f.pth' % (opt.outf, epoch, total_loss))
+            total_loss2 += loss.item()
+        total_loss2 /= len(valid_dataset)
+        print(blue('test: epoch %d, average loss: %f' % (epoch, total_loss2)))
+        # with open(opt.outf + '/log.txt', 'a') as f:
+        #     f.write('epoch: %d, loss: %f\n' % (epoch, total_loss2))
+        if min_loss > total_loss1:
+            min_loss = total_loss1
+            if epoch >= 10:
+                torch.save(classifier.state_dict(), '%s/model_%d_%f.pth' % (opt.outf, epoch, total_loss1))
 
     # total_correct = 0
     # total_testset = 0
