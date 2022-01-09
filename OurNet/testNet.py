c661f8b2aedace4cbd7ffd85c49831bcaa111a8a
zrh_local <zrh735847540@outlook.com>
2022-01-09 13:06:39 +0800

    update data (fix number)
    (cherry picked from commit 76d745a23474de1d8d49b834f6f39d414b340037)

---
+++ OurNet/testNet.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PointNetCls(nn.Module):
+    def __init__(self, k=4):
+        super(PointNetCls, self).__init__()
+        # self.feat = PointNetfeat()
+        self.fc1 = nn.Linear(2048, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 2)
+        self.fc5 = nn.Linear(512, 256)
+        self.fc6 = nn.Linear(256, k)
+        self.dropout = nn.Dropout(p=0.3)
+        # self.bn1 = nn.BatchNorm1d(512)
+        # self.bn2 = nn.BatchNorm1d(256)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # x1, trans1, trans_feat1 = self.feat(x1)
+        # x2, trans2, trans_feat2 = self.feat(x2)
+        # x = torch.cat((x1, x2), 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        out1 = F.relu(self.fc3(x))
+        out1 = F.sigmoid(self.fc4(out1))
+        # out2 = F.relu(self.fc5(x))
+        # out2 = self.fc6(out2)
+        return out1
