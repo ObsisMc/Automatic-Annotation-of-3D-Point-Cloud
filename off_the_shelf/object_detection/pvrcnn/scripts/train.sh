33dc8e6056148c080799d238cd65a6cb17039fbd
zrh_lie <735847540@qq.com>
2022-03-01 03:04:54 +0000

    updata train.sh

--- off_the_shelf/object_detection/pvrcnn/scripts/train.sh
+++ off_the_shelf/object_detection/pvrcnn/scripts/train.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-# this for training pvrcnn
-path='/home2/lie/InnovativePractice2/OpenPCDet/tools' # root of pvrcnn project
-python train.py --cfg_file ${path}/cfgs/kitti_models/pv_rcnn.yaml --pretrained_model ${path}/../pv_rcnn_8369.pth
+# this for training pvrcnn, it must be placed in tools file
+path=. # root of tools of pvrcnn project
+nohupout=${path}/../output/nohups/train.out
+nohup python ${path}/train.py --cfg_file ${path}/cfgs/kitti_models/pv_rcnn.yaml --pretrained_model ${path}/../pv_rcnn_8369.pth > ${nohupout} 2>&1 &
