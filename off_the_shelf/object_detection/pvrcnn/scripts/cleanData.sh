209d330cbc9096690964454c5817d624d786a1dc
zrh_lie <735847540@qq.com>
2022-03-01 07:38:50 +0000

    update clean and importdata sh

--- off_the_shelf/object_detection/pvrcnn/scripts/cleanData.sh
+++ off_the_shelf/object_detection/pvrcnn/scripts/cleanData.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
+# rm all testing and training data in openpcdet
 path=/home2/lie/InnovativePractice2/OpenPCDet/data/kitti/
 rm -r ${path}/testing
 rm -r ${path}/training
-mkdir testing
-mkdir training
+mkdir ${path}/testing
+mkdir ${path}/training
--- off_the_shelf/object_detection/pvrcnn/scripts/importData.sh
+++ off_the_shelf/object_detection/pvrcnn/scripts/importData.sh
@@ -1,14 +1,43 @@
 #! /bin/bash
+# dir of openpcdet
 testing=~/InnovativePractice2/OpenPCDet/data/kitti/testing
 training=~/InnovativePractice2/OpenPCDet/data/kitti/training
+# dir of data
+test_data_calib=/public_dataset/kitti/object/data_object_calib/testing/calib
 test_data_velodyne=/public_dataset/kitti/object/data_object_velodyne/testing/velodyne
 test_data_image=/public_dataset/kitti/object/data_object_image_2/testing/image_2
+
+train_data_calib=/public_dataset/kitti/object/data_object_calib/training/calib
+train_data_velodyne=/public_dataset/kitti/object/data_object_velodyne/training/velodyne
+train_data_image=/public_dataset/kitti/object/data_object_image_2/training/image_2
 echo "Begin import data into OpenPcdet..."
-echo "Import testing data to $testing:"
+# testing
+echo "Import testing calib to $testing:"
+echo "cp $test_data_calib"
+cp -r $test_data_calib $testing
+echo "Finish testing calib!"
+
+echo "Import testing velodyne to $testing:"
 echo "cp $test_data_velodyne"
 cp -r $test_data_velodyne $testing
 echo "Finish testing velodyne!"
+
+echo "Import testing image to $testing:"
 echo "cp $test_data_image"
 cp -r $test_data_image $testing
 echo "Finish testing image!"
+# training
+echo "Import training calib to ${training}:"
+echo "cp ${train_data_calib}"
+cp -r ${train_data_calib} ${training}
+echo "Finish training calib!"
 
+echo "Import training velodyne to ${training}:"
+echo "cp ${train_data_velodyne}"
+cp -r ${train_data_velodyne} ${training}
+echo "Finish training velodyne!"
+
+echo "Import training image to ${training}:"
+echo "cp ${train_data_image}"
+cp -r ${train_data_image} ${training}
+echo "Finish training image!"
