6e72d8789e0341e5e833cdee11627301199205f7
zrh_lie <735847540@qq.com>
2022-03-06 10:47:21 +0000

    finish object2AB3dMOT and remove generateImagesSets

--- util/format_util/object2AB3dMOT.py
+++ util/format_util/object2AB3dMOT.py
@@ -1,8 +1,8 @@
 import os
 
-object_result = "../test/object/training/label_2/"
-splitpos = "../test/object/training/splitpos.txt"
-output = "../test/tracking/label_2/"
+object_result = "/home2/lie/InnovativePractice2/OpenPCDet/output/cfgs/kitti_models/pv_rcnn/default/eval/epoch_8369/val/default/final_result/data"
+splitpos = "/home2/lie/InnovativePractice2/OpenPCDet/data/kitti/training/splitpos.txt"
+output = "/home2/lie/InnovativePractice2/OpenPCDet/data/kitti/training/tracking_label"
 
 
 def parserType(tp):
@@ -20,24 +20,26 @@ def parserLable():
 
 
 def splitPosition():
-    try:
-        with open(splitpos, 'r') as f:
-            lines = f.read().split("\n")
-            return [int(i) for i in lines]
-    except:
-        return 0
+    with open(splitpos, 'r') as f:
+        lines = f.read().rstrip("\n").split("\n")
+        splitp = []
+        for line in lines:
+            if line == '':
+                continue
+            splitp.append(int(line))
+        return splitp
 
 
 def main():
     splitp = splitPosition()
     if not splitp:
-        print("Cannot find splitpos.txt")
         return
     if not os.path.exists(output):
         os.mkdir(output)
 
     files = sorted(os.listdir(object_result), key=lambda x: int(x.rstrip(".txt")))
     p = 0
+    baseframe = 0
     for scene in range(len(splitp)):
         endframe = splitp[scene]
         with open(os.path.join(output, "{:04d}.txt".format(scene)), 'w') as f:
@@ -52,11 +54,12 @@ def main():
                         if label == '':
                             continue
                         label = label.split(" ")
-                        framelabel = "{} {} {} {} {} {}".format(frame, parserType(label[0]),
+                        framelabel = "{} {} {} {} {} {}".format(frame - baseframe, parserType(label[0]),
                                                                 " ".join(label[4:8]), label[-1],
                                                                 "".join(label[8:15]), label[3])
                         f.write(framelabel + '\n')
                 p += 1
+        baseframe = endframe
 
 
 if __name__ == "__main__":
