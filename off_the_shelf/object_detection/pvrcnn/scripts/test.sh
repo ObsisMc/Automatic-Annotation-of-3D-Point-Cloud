4b55a10fc0890a72010e669f6726ce63cccc8d5e
zrh_lie <735847540@qq.com>
2022-03-10 11:52:59 +0000

    update test.sh in off_the_shelf/openpcd

--- off_the_shelf/object_detection/pvrcnn/scripts/test.sh
+++ off_the_shelf/object_detection/pvrcnn/scripts/test.sh
@@ -1,6 +1,24 @@
 #! /bin/bash
+ptplr_cfg=pointpillar.yaml
+ptplr_ckpt=pointpillar_7728.pth
+
+pvrcnn_cfg=pv_rcnn.yaml
+pvrcnn_ckpt=pv_rcnn_8369.pth
+
+ptrcnn_cfg=pointrcnn.yaml
+ptrcnn_ckpt=pointrcnn_7870.pth
+
+scnd_cfg=second.yaml
+scnd_ckpt=second_7862.pth
+
+vxlrcnn_cfg=voxel_rcnn_car.yaml
+vxlrcnn_ckpt=voxel_rcnn_car_84.54.pth
+
+prtA2_fr_cfg=PartA2_free.yaml
+prtA2_fr_ckpt=PartA2_free_7872.pth
+
 path=.
-CONFIG_FILE=${path}/cfgs/kitti_models/pv_rcnn.yaml
-BATCH_SIZE=2
-CKPT=../pv_rcnn_8369.pth
-python test.py --cfg_file ${CONFIG_FILE} --batch_size ${BATCH_SIZE} --ckpt ${CKPT}
+CONFIG_FILE=${path}/cfgs/kitti_models/${prtA2_fr_cfg}
+BATCH_SIZE=1
+CKPT=../models/${prtA2_fr_ckpt}
+python test.py --cfg_file ${CONFIG_FILE} --batch_size ${BATCH_SIZE} --ckpt ${CKPT}  --save_to_file
