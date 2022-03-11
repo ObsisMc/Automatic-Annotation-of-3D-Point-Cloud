1809402b7a37e41e31283e074d321779dfec14cd
zrh-LegionLinux <735847540@qq.com>
2022-03-11 19:15:14 +0800

    update visualize module

--- util/visual_utils/visualizer.py
+++ util/visual_utils/visualizer.py
@@ -1,17 +1,23 @@
 from visual_modul import open3d_vis_utils as V
+from visual_modul.calibration import Calibration
 import numpy as np
 
 basepath = "../../"
-pointspath = basepath + "test/training/velodyne/0000/00000.bin"
+pointspath = basepath + "test/training/velodyne/0000/000000.bin"
 boxpath = basepath + "test/training/label/000000.txt"
+calibpath = basepath + "test/training/calib/0000.txt"
 
 
 def load_points():
     points = np.fromfile(pointspath, dtype=np.float32).reshape(-1, 4)
-    return points[:, :3]
+    return points[:, :]
 
 
 def load_boxes_from_object_txt():
+    """
+    1. the boxes should be calibrated
+    2. box should be [location + l + h + w + angle]
+    """
     boxes = []
     with open(boxpath, 'r') as f:
         lines = f.readlines()
@@ -19,7 +25,7 @@ def load_boxes_from_object_txt():
             line = line.rstrip("\n").split(" ")
             if line[0] == "DontCare":
                 continue
-            box = line[11:14] + [line[10], line[9], line[8]] + [line[14]]
+            box = line[11:14] + [line[10], line[8], line[9]] + [line[14]]
             boxes.append(box)
     return np.array(boxes, dtype=np.float32)
 
@@ -31,8 +37,11 @@ def load_boxes_from_object_txt():
 
 
 if __name__ == '__main__':
+    calibration = Calibration(calibpath)
+
     points = load_points()
     boxes = load_boxes_from_object_txt()
+    boxes = calibration.bbox_rect_to_lidar(boxes)
     V.draw_scenes(
         points=points, ref_boxes=boxes
     )
