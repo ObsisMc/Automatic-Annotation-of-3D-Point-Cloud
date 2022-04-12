971406e61139b20aa5e077b8a8524fa707724edb
zrh_lie <735847540@qq.com>
2022-04-12 07:05:09 +0000

    update test.sh and train.sh

--- off_the_shelf/object_detection/pvrcnn/scripts/test.sh
+++ off_the_shelf/object_detection/pvrcnn/scripts/test.sh
@@ -3,7 +3,7 @@ ptplr_cfg=pointpillar.yaml
 ptplr_ckpt=pointpillar_7728.pth
 
 pvrcnn_cfg=pv_rcnn.yaml
-pvrcnn_ckpt=pv_rcnn_8369.pth
+pvrcnn_ckpt=pv_rcnn_epoch_30.pth
 
 ptrcnn_cfg=pointrcnn.yaml
 ptrcnn_ckpt=pointrcnn_7870.pth
@@ -18,7 +18,7 @@ prtA2_fr_cfg=PartA2_free.yaml
 prtA2_fr_ckpt=PartA2_free_7872.pth
 
 path=.
-CONFIG_FILE=${path}/cfgs/kitti_models/${prtA2_fr_cfg}
-BATCH_SIZE=1
-CKPT=../models/${prtA2_fr_ckpt}
+CONFIG_FILE=${path}/cfgs/kitti_models/${pvrcnn_cfg}
+BATCH_SIZE=4
+CKPT=../models/${pvrcnn_ckpt}
 python test.py --cfg_file ${CONFIG_FILE} --batch_size ${BATCH_SIZE} --ckpt ${CKPT}  --save_to_file
--- off_the_shelf/object_detection/pvrcnn/scripts/train.sh
+++ off_the_shelf/object_detection/pvrcnn/scripts/train.sh
@@ -2,4 +2,5 @@
 # this for training pvrcnn, it must be placed in tools file
 path=. # root of tools of pvrcnn project
 nohupout=${path}/../output/nohups/train.out
-nohup python ${path}/train.py --cfg_file ${path}/cfgs/kitti_models/pv_rcnn.yaml --pretrained_model ${path}/../pv_rcnn_8369.pth > ${nohupout} 2>&1 &
+
+nohup python ${path}/train.py --cfg_file ${path}/cfgs/kitti_models/pv_rcnn.yaml --pretrained_model ${path}/../models/pv_rcnn_8369.pth --batch_size 1 > ${nohupout} 2>&1 &
