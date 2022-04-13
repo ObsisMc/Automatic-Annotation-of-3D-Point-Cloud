6cf1624f61327f76bb4aab08e0319924c2205124
zrh-LegionLinux <735847540@qq.com>
2022-04-13 18:12:52 +0800

    add objectLabel2Tracking.py

---
+++ util/format_utils/objectLabel2Tracking.py
@@ -0,0 +1,58 @@
+# You should use test.py with '-save_to_file' to get txt firstly
+import os
+
+object_result = "/home2/lie/InnovativePractice2/OpenPCDet/output/cfgs/kitti_models/pv_rcnn/default/eval/epoch_30/val/default/final_result/data"
+splitpos = "/home2/lie/InnovativePractice2/OpenPCDet/data/kitti/training/splitpos.txt"
+output = "/home2/lie/InnovativePractice2/OpenPCDet/data/kitti/tracking_label/kitti/"
+
+
+def parserLable():
+    pass
+
+
+def splitPosition():
+    with open(splitpos, 'r') as f:
+        lines = f.read().rstrip("\n").split("\n")
+        splitp = []
+        for line in lines:
+            if line == '':
+                continue
+            splitp.append(int(line))
+        return splitp
+
+
+def main(threshold=0.6):
+    splitp = splitPosition()
+    if not splitp:
+        return
+    if not os.path.exists(output):
+        os.mkdir(output)
+
+    files = sorted(os.listdir(object_result), key=lambda x: int(x.rstrip(".txt")))
+    p = 0
+    baseframe = 0
+    for scene in range(len(splitp)):
+        endframe = splitp[scene]
+        with open(os.path.join(output, "{:04d}.txt".format(scene)), 'w') as f:
+            while p < len(files):
+                frame = int(files[p].rstrip(".txt"))
+                if frame >= endframe:
+                    break
+                with open(os.path.join(object_result, files[p]), 'r') as labeltxt:
+                    labels = labeltxt.read().split("\n")
+                    for label in labels:
+                        # data clean
+                        if label == '':
+                            continue
+                        label = label.split(" ")
+                        # filter low confidence
+                        if float(label[-1]) < threshold:
+                            continue
+                        framelabel = "{} {} {}".format(frame - baseframe, label[0], label[1:])
+                        f.write(framelabel + '\n')
+                p += 1
+        baseframe = endframe
+
+
+if __name__ == "__main__":
+    main()
