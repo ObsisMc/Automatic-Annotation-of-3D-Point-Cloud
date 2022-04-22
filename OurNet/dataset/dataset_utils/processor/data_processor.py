54f53423ef398868a1e76844416baa2cdbbaa711
zrh-LegionLinux <735847540@qq.com>
2022-04-22 22:39:57 +0800

    Add Beta of bev generation of Openpcdet
    Transplant data_processor.py and pillar_vfe.py from Openpcdet and primary test has passed

---
+++ OurNet/dataset/dataset_utils/processor/data_processor.py
@@ -0,0 +1,130 @@
+from functools import partial
+
+import numpy as np
+from skimage import transform
+
+tv = None
+try:
+    import cumm.tensorview as tv
+except:
+    pass
+
+
+class VoxelGeneratorWrapper():
+    def __init__(self, vsize_xyz, coors_range_xyz, num_point_features, max_num_points_per_voxel, max_num_voxels):
+        try:
+            from spconv.utils import VoxelGeneratorV2 as VoxelGenerator
+            self.spconv_ver = 1
+        except:
+            try:
+                from spconv.utils import VoxelGenerator
+                self.spconv_ver = 1
+            except:
+                from spconv.utils import Point2VoxelCPU3d as VoxelGenerator
+                self.spconv_ver = 2
+
+        if self.spconv_ver == 1:
+            self._voxel_generator = VoxelGenerator(
+                voxel_size=vsize_xyz,
+                point_cloud_range=coors_range_xyz,
+                max_num_points=max_num_points_per_voxel,
+                max_voxels=max_num_voxels
+            )
+        else:
+            self._voxel_generator = VoxelGenerator(
+                vsize_xyz=vsize_xyz,
+                coors_range_xyz=coors_range_xyz,
+                num_point_features=num_point_features,
+                max_num_points_per_voxel=max_num_points_per_voxel,
+                max_num_voxels=max_num_voxels
+            )
+
+    def generate(self, points):
+        if self.spconv_ver == 1:
+            voxel_output = self._voxel_generator.generate(points)
+            if isinstance(voxel_output, dict):
+                voxels, coordinates, num_points = \
+                    voxel_output['voxels'], voxel_output['coordinates'], voxel_output['num_points_per_voxel']
+            else:
+                voxels, coordinates, num_points = voxel_output
+        else:
+            assert tv is not None, f"Unexpected error, library: 'cumm' wasn't imported properly."
+            voxel_output = self._voxel_generator.point_to_voxel(tv.from_numpy(points))
+            tv_voxels, tv_coordinates, tv_num_points = voxel_output
+            # make copy with numpy(), since numpy_view() will disappear as soon as the generator is deleted
+            voxels = tv_voxels.numpy()
+            coordinates = tv_coordinates.numpy()
+            num_points = tv_num_points.numpy()
+        return voxels, coordinates, num_points
+
+
+class DataProcessor(object):
+    def __init__(self, point_cloud_range, training, num_point_features):
+        self.point_cloud_range = point_cloud_range
+        self.training = training
+        self.num_point_features = num_point_features
+        self.mode = 'train' if training else 'test'
+        self.grid_size = self.voxel_size = None
+        self.data_processor_queue = []
+
+        self.voxel_generator = None
+
+    def transform_points_to_voxels(self, data_dict=None, config=None):
+        # config
+        if data_dict is None:
+            grid_size = (self.point_cloud_range[3:6] - self.point_cloud_range[0:3]) / np.array(config.VOXEL_SIZE)
+            self.grid_size = np.round(grid_size).astype(np.int64)
+            self.voxel_size = config.VOXEL_SIZE
+            # just bind the config, we will create the VoxelGeneratorWrapper later,
+            # to avoid pickling issues in multiprocess spawn
+            return partial(self.transform_points_to_voxels, config=config)
+
+        if self.voxel_generator is None:
+            self.voxel_generator = VoxelGeneratorWrapper(
+                vsize_xyz=config.VOXEL_SIZE,
+                coors_range_xyz=self.point_cloud_range,
+                num_point_features=self.num_point_features,
+                max_num_points_per_voxel=config.MAX_POINTS_PER_VOXEL,
+                max_num_voxels=config.MAX_NUMBER_OF_VOXELS[self.mode],
+            )
+
+        points = data_dict['points']
+        voxel_output = self.voxel_generator.generate(points)
+        voxels, coordinates, num_points = voxel_output
+
+        data_dict['voxels'] = voxels
+        data_dict['voxel_coords'] = coordinates
+        data_dict['voxel_num_points'] = num_points
+        return data_dict
+
+
+class Config():
+    def __init__(self):
+        self.VOXEL_SIZE = [0.16, 0.16, 4.0]
+        self.MAX_POINTS_PER_VOXEL = 32
+        self.MAX_NUMBER_OF_VOXELS = {"test": 10000, "train": 160000}
+
+
+def init_data_dict():
+    plst = [[0.8, 0.08, 1, 1]]
+    # plst = [[0.0, 0.0, 0.0, 1]]
+    points = np.array(plst)
+    return {"points": np.array(points)}  # need np.darray
+
+
+def getDataDict():
+    data_dict = init_data_dict()
+    config = Config()
+    range = np.array([0.0, 0.0, 0.0, 8, 1.6, 4.0])
+
+    data_dict["image_shape"] = (range[3:6] - range[0:3]) / np.array(config.VOXEL_SIZE)
+    dataprocesspr = DataProcessor(range, 1, 4)
+    # dataprocesspr = DataProcessor(np.array([0, 0, 0, 0.16, 0.16, 4]), 1, 4)
+    data_dict = dataprocesspr.transform_points_to_voxels(data_dict=data_dict, config=config)
+    return data_dict, range
+
+
+if __name__ == "__main__":
+    # it may be used before dataloader
+    data_dict = getDataDict()
+    print(data_dict)
